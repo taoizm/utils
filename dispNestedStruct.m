9846ce2d329d654fbd3e1449da7a18dc214474af
Kentaro Tao <kentaro.tao@gmail.com>
2023-04-19 15:54:21 +0900

    Update dispNestedStruct.m

--- dispNestedStruct.m
+++ dispNestedStruct.m
@@ -3,49 +3,53 @@ function dispNestedStruct(S)
 %   dispNestedStruct(S) displays the content of a nested struct array S.
 %
 %   See also disp.
-%
+
 %   Copyright (c) 2023 Kentaro Tao
 %   Released under the MIT license.
 %   See https://opensource.org/license/mit/
-    
+
 arguments
     S struct {mustBeScalarOrEmpty(S)}
 end
-    
+
 fprintf(getFormattedStr(S));
-fprintf('\n')
-    
+
+s = settings;
+if strcmp(s.matlab.commandwindow.DisplayLineSpacing.ActiveValue,'loose')
+    fprintf('\n');
+end
+
 end % dispNestedStruct
-    
-    
+
+
 %% Private functions
-function formattedStr = getFormattedStr(S,depth,fieldWidth)
+function formattedStr = getFormattedStr(S,level,fieldWidth)
     % GETSTRARRAY Recursively extracts the formatted display text
     %     of a nested struct and returns a formatted string.
     arguments
         S struct {mustBeScalarOrEmpty(S)}
-        depth = 1
+        level = 1
         fieldWidth = getFieldWidth(S)
     end
     
     formattedTextArray = strsplit(evalc('disp(S)'),'\n');
     formattedStr = '';
-    
-    fieldNames = fieldnames(S);
-    
-    for ii=1:numel(fieldNames)
+
+    fields = fieldnames(S);
+
+    for ii=1:numel(fields)
         str = formattedTextArray{ii};
-        padding = repmat(' ', 1, fieldWidth(depth) - strfind(str,':') + 1);
+        padding = repmat(' ', 1, fieldWidth(level) - strfind(str,':') + 1);
         formattedStr = [formattedStr padding str '\n'];
-    
+
         % Recursively extract the field names
-        if isstruct(S.(fieldNames{ii}))
+        if isstruct(S.(fields{ii}))
             formattedStr = [formattedStr ...
-                getFormattedStr(S.(fieldNames{ii}),depth+1,fieldWidth)];
+                getFormattedStr(S.(fields{ii}),level+1,fieldWidth)];
         end
     end
 end % getStrArray
-    
+
 function fieldWidth = getFieldWidth(S)
     %GETFIELDWIDTH
     arguments
@@ -54,32 +58,32 @@ function fieldWidth = getFieldWidth(S)
     
     maxLength = getMaxLength(S);
     nestDepth = length(maxLength);
-    
+
     fieldWidth = 4 * (1:nestDepth); % indent width is fixed to 4
     fieldWidth = 4 + fieldWidth + max(maxLength-fieldWidth);
 end % getFieldWidth
-    
-function maxLength = getMaxLength(S,depth,maxLength)
+
+function maxLength = getMaxLength(S,level,maxLength)
     %GETMAXLENGTH Finds max field name length at each nested struct level.
     %   maxLength = getMaxLength(S) returns a vector containing the maximum
     %   length of field names at each level of the nested struct S.
     arguments
         S struct
-        depth = 1
+        level = 1
         maxLength = []
     end
-
-    fieldNames = fieldnames(S);
     
-    if depth > length(maxLength)
-        maxLength(depth) = 0;
+    fields = fieldnames(S);
+
+    if level > length(maxLength)
+        maxLength(level) = 0;
     end
     
-    maxLength(depth) = max(max(cellfun(@length,fieldNames)),maxLength(depth));
-    
-    for ii=1:length(fieldNames)
-        if isstruct(S.(fieldNames{ii}))
-            maxLength = getMaxLength(S.(fieldNames{ii}),depth+1,maxLength);
+    maxLength(level) = max(max(cellfun(@length,fields)),maxLength(level));
+
+    for ii=1:length(fields)
+        if isstruct(S.(fields{ii}))
+            maxLength = getMaxLength(S.(fields{ii}),level+1,maxLength);
         end
     end
 end % getMaxLength
